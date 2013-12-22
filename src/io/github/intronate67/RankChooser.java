11b7abdf696f46060d09dc00a313697a4de0b181
intronate67 <huntersharpe@me.com>
2013-12-22 12:50:59 -0600

    Fixed Spacing error.

--- src/io/github/intronate67/RankChooser.java
+++ src/io/github/intronate67/RankChooser.java
@@ -2,30 +2,6 @@ package io.github.intronate67;
 
 import java.util.logging.Logger;
 
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
-
-public class RankChooser extends JavaPlugin{
-	public final Logger logger = Logger.getLogger("Minecraft");
-	public static RankChooser plugin;
-	public int version = (int) 1.0;
-
-	@Override
-	public void onEnable(){
-		getLogger().info("Rank Chooser" + version + " has been disabled!");
-	}
-	
-	@Override
-	public void onDisable(){
-		getLogger().info("Rank Chooser" + version + " has been disabled!");
-	}
-package io.github.intronate67;
-
-import java.util.logging.Logger;
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
