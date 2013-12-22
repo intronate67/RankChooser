d69011cec4a6d9be30360f8a2d996148806e4ffe
intronate67 <huntersharpe@me.com>
2013-12-22 12:49:58 -0600

    Improved functionality of Else statement.
    Added Bukkit.broadcast() to broadcast the Players Rank Choice to Staff members that we have given the permission fuckbitches.getmoney.

--- src/io/github/intronate67/RankChooser.java
+++ src/io/github/intronate67/RankChooser.java
@@ -22,12 +22,36 @@ public class RankChooser extends JavaPlugin{
 	public void onDisable(){
 		getLogger().info("Rank Chooser" + version + " has been disabled!");
 	}
+package io.github.intronate67;
+
+import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RankChooser extends JavaPlugin{
+	public final Logger logger = Logger.getLogger("Minecraft");
+	public static RankChooser plugin;
+	public int version = (int) 1.0;
+
+	@Override
+	public void onEnable(){
+		getLogger().info("Rank Chooser" + version + " has been enabled!");
+	}
 	
-		public boolean onCommand(CommandSender sender, Command cmd, String lable, String[] args){
+	@Override
+	public void onDisable(){
+		getLogger().info("Rank Chooser" + version + " has been disabled!");
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String lable, String[] args){
 		Player player = (Player) sender;
-		Player target = Bukkit.getPlayer(args[1]);
 		if(cmd.getName().equalsIgnoreCase("chooserank")){
-			if(args.length < 10){
+			if(args.length < 1){
 				player.sendMessage(ChatColor.BLUE + "Available Ranks:");
 				player.sendMessage(ChatColor.DARK_BLUE + "Architect");
 				player.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.DARK_AQUA + "Student");
@@ -49,7 +73,7 @@ public class RankChooser extends JavaPlugin{
 				player.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.DARK_PURPLE + "Skillful");
 				player.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.GREEN + "Master");
 			}else{
-				Bukkit.broadcast(sender.getName() + target.getName() , "fuckbitches.getmoney");
+				Bukkit.broadcast(ChatColor.BLUE + "[ChooseRank] " + ChatColor.AQUA + "Player: " + ChatColor.ITALIC + player.getDisplayName() + ChatColor.AQUA + " Would like to choose the Rank: " ,"fuckbitches.getmoney");
 			}
 		}
 			
