b0a9112d58dcc31e99fa8bda0e4ee4c7d4202d99
Hunter Sharpe <huntersharpe@me.com>
2013-12-23 14:57:44 -0600

    Added Config.yml Created + more

--- src/io/github/intronate67/RankChooser.java
+++ src/io/github/intronate67/RankChooser.java
@@ -1,5 +1,6 @@
 package io.github.intronate67;
 
+import java.io.File;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
@@ -13,11 +14,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class RankChooser extends JavaPlugin{
 	public final Logger logger = Logger.getLogger("Minecraft");
 	public static RankChooser plugin;
-	public int version = (int) 1.0;
+	public int version = (int) 1.1;
 	public String author = "intronate67";
 
 	@Override
 	public void onEnable(){
+		//Creating Config.yml
+		if(!new File(this.getDataFolder(), "config.yml").exists()){
+			this.saveDefaultConfig();
+		}
+		//Displays server on/off
 		getLogger().info("Rank Chooser" + version + " has been enabled!");
 	}
 	
@@ -31,20 +37,20 @@ public class RankChooser extends JavaPlugin{
 		if(cmd.getName().equalsIgnoreCase("chooserank")){
 			if(args.length < 1){
 				player.sendMessage(ChatColor.BLUE + "Available Ranks:");
-				player.sendMessage(ChatColor.DARK_BLUE + "Architect");
+				player.sendMessage(getConfig().getString("rank1"));
 				player.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.DARK_AQUA + "Student");
 				player.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.YELLOW + "Apprentice");
 				player.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.DARK_PURPLE + "Skillful");
 				player.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.GREEN + "Master");
 				player.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.AQUA + "Legendary");
-				player.sendMessage(ChatColor.GOLD + "Explorer");
+				player.sendMessage(getConfig().getString("rank2"));
 				player.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.DARK_BLUE + "Architect");
 				player.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.DARK_AQUA + "Student");
 				player.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.YELLOW + "Apprentice");
 				player.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.DARK_PURPLE + "Skillful");
 				player.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.GREEN + "Master");
 				player.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.AQUA + "Legendary");
-				player.sendMessage(ChatColor.RED + "Redstoner");
+				player.sendMessage(getConfig().getString("rank3"));
 				player.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.DARK_BLUE + "Architect");
 				player.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.DARK_AQUA + "Student");
 				player.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.YELLOW + "Apprentice");
@@ -63,25 +69,25 @@ public class RankChooser extends JavaPlugin{
 			player.sendMessage(ChatColor.BLUE + "https://github.com/intronate67/RankChooser or use /cshelp");
 		}
 		if(cmd.getName().equalsIgnoreCase("crhelp")){
-			player.sendMessage(ChatColor.DARK_AQUA + "Commands" + ChatColor.DARK_GRAY + "Page 1 of 4");
+			player.sendMessage(ChatColor.DARK_AQUA + "Commands" + ChatColor.DARK_GRAY + " Page 1 of 4");
 			player.sendMessage(ChatColor.AQUA + "/chooserank");
 			player.sendMessage(ChatColor.AQUA + "/cs");
 			player.sendMessage(ChatColor.AQUA + "/cshelp");
 		}
 		if(cmd.getName().equalsIgnoreCase("crhelp 2")){
-			player.sendMessage(ChatColor.DARK_AQUA + "/ChooseRank" + ChatColor.DARK_GRAY + "Page 2 of 4");
+			player.sendMessage(ChatColor.DARK_AQUA + "/ChooseRank" + ChatColor.DARK_GRAY + " Page 2 of 4");
 			player.sendMessage(ChatColor.AQUA + "Usage: /chooserank <rank>");
 			player.sendMessage(ChatColor.AQUA + "Functionality: Sends message ");
 			player.sendMessage(ChatColor.AQUA + "to you and staff with your rank request.");
 		}
 		if(cmd.getName().equalsIgnoreCase("crhelp 3")){
-			player.sendMessage(ChatColor.DARK_AQUA + "/cr" + ChatColor.DARK_GRAY + "Page 3 of 4");
+			player.sendMessage(ChatColor.DARK_AQUA + "/cr" + ChatColor.DARK_GRAY + " Page 3 of 4");
 			player.sendMessage(ChatColor.AQUA + "Usage: /cr");
 			player.sendMessage(ChatColor.AQUA + "Functionality: Displays all of the  ");
 			player.sendMessage(ChatColor.AQUA + "plugin info inlcuding Version and Author.");
 		}
 		if(cmd.getName().equalsIgnoreCase("crhelp 4")){
-			player.sendMessage(ChatColor.DARK_AQUA + "/CrHelp" + ChatColor.DARK_GRAY + "Page 4 of 4");
+			player.sendMessage(ChatColor.DARK_AQUA + "/CrHelp" + ChatColor.DARK_GRAY + " Page 4 of 4");
 			player.sendMessage(ChatColor.AQUA + "Usage: /crshelp");
 			player.sendMessage(ChatColor.AQUA + "Functionality: Displays all of the commands ");
 			player.sendMessage(ChatColor.AQUA + "and four pages about them.");
